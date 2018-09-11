acceabce9fc47047d23f5ce2c7da71683e53f3a9
pathikmehta <pathikmehta11@gmail.com>
2018-09-11 11:38:06 +0530

    Added default initial implementaiton of start acceptor and start intiator

--- FxFixAcceptor/src/main/java/com/pirimid/fxfix/StartAcceptor.java
+++ FxFixAcceptor/src/main/java/com/pirimid/fxfix/StartAcceptor.java
@@ -1,8 +1,24 @@
 package com.pirimid.fxfix;
 
+import quickfix.*;
+
 public class StartAcceptor {
 
     public static void main(String args[]) {
-        System.out.println("Start Acceptor main method");
+        SocketAcceptor socketAcceptor = null;
+        try {
+            SessionSettings executorSettings = new SessionSettings(
+                    "./acceptorSettings.txt");
+            Application application = new FxFixAcceptor();
+            FileStoreFactory fileStoreFactory = new FileStoreFactory(
+                    executorSettings);
+            MessageFactory messageFactory = new DefaultMessageFactory();
+            FileLogFactory fileLogFactory = new FileLogFactory(executorSettings);
+            socketAcceptor = new SocketAcceptor(application, fileStoreFactory,
+                    executorSettings, fileLogFactory, messageFactory);
+            socketAcceptor.start();
+        } catch (ConfigError configError) {
+            configError.printStackTrace();
+        }
     }
 }
--- FxFixInitiator/src/main/java/com/pirimid/fxfix/StartInitiator.java
+++ FxFixInitiator/src/main/java/com/pirimid/fxfix/StartInitiator.java
@@ -1,8 +1,65 @@
 package com.pirimid.fxfix;
 
+import quickfix.*;
+import quickfix.field.*;
+import quickfix.fix42.Logon;
+import quickfix.fix42.NewOrderSingle;
+
 public class StartInitiator {
 
     public static void main(String args[]) {
-        System.out.println("Strart initiator main method");
+        SocketInitiator socketInitiator = null;
+        try {
+            SessionSettings initiatorSettings = new SessionSettings(
+                    "./initiatorSettings.txt");
+            Application initiatorApplication = new FxFixInitiator();
+            FileStoreFactory fileStoreFactory = new FileStoreFactory(
+                    initiatorSettings);
+            FileLogFactory fileLogFactory = new FileLogFactory(
+                    initiatorSettings);
+            MessageFactory messageFactory = new DefaultMessageFactory();
+            socketInitiator = new SocketInitiator(initiatorApplication, fileStoreFactory, initiatorSettings, fileLogFactory, messageFactory);
+            socketInitiator.start();
+            SessionID sessionId =  (SessionID) socketInitiator.getSessions().get(0);
+            Session.lookupSession(sessionId).logon();
+
+
+            Logon logon = new Logon();
+
+            logon.set(new quickfix.field.HeartBtInt(30));
+            logon.set(new quickfix.field.ResetSeqNumFlag(true));
+            logon.set(new quickfix.field.EncryptMethod(0));
+
+            try {
+                Session.sendToTarget(logon, sessionId);
+            } catch (SessionNotFound sessionNotFound) {
+                sessionNotFound.printStackTrace();
+            }
+
+
+            for(int j = 0; j < 2; j ++){
+                try {
+                    Thread.sleep(10000);
+                    NewOrderSingle newOrderSingle = new NewOrderSingle(
+                            new ClOrdID("456"),
+                            new HandlInst('3'),
+                            new Symbol("AJCB"),
+                            new Side(Side.BUY),
+                            new TransactTime(),
+                            new OrdType(OrdType.MARKET)
+                    );
+                    System.out.println("####New Order Sent :" + newOrderSingle.toString());
+                    Session.sendToTarget(newOrderSingle, sessionId);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (SessionNotFound sessionNotFound) {
+                    sessionNotFound.printStackTrace();
+                }
+            }
+
+        }  catch (ConfigError configError) {
+            configError.printStackTrace();
+        }
     }
 }
