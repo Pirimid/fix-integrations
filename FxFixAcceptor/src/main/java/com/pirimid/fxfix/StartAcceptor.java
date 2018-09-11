7554ee751646e8cecb640f2bb4142e526897a92b
pathikmehta <pathikmehta11@gmail.com>
2018-09-11 10:58:33 +0530

    Added start acceptor and start initiator with initial main method

---
+++ FxFixAcceptor/src/main/java/com/pirimid/fxfix/StartAcceptor.java
@@ -0,0 +1,8 @@
+package com.pirimid.fxfix;
+
+public class StartAcceptor {
+
+    public static void main(String args[]) {
+        System.out.println("Start Acceptor main method");
+    }
+}
---
+++ FxFixInitiator/src/main/java/com/pirimid/fxfix/StartInitiator.java
@@ -0,0 +1,8 @@
+package com.pirimid.fxfix;
+
+public class StartInitiator {
+
+    public static void main(String args[]) {
+        System.out.println("Strart initiator main method");
+    }
+}
