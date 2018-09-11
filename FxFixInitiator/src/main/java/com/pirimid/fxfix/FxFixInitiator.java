f4d481e40d7f18acdc08dd3f97e407f078607374
pathikmehta <pathikmehta11@gmail.com>
2018-09-11 11:32:36 +0530

    Added default initial implmentation of FxFixAcceptor and FxFixInitiator

---
+++ FxFixInitiator/src/main/java/com/pirimid/fxfix/FxFixInitiator.java
@@ -0,0 +1,42 @@
+package com.pirimid.fxfix;
+
+import quickfix.*;
+
+public class FxFixInitiator extends MessageCracker implements Application {
+
+    @Override
+    public void onCreate(SessionID sessionID) {
+
+    }
+
+    @Override
+    public void onLogon(SessionID sessionID) {
+
+    }
+
+    @Override
+    public void onLogout(SessionID sessionID) {
+
+    }
+
+    @Override
+    public void toAdmin(Message message, SessionID sessionID) {
+
+    }
+
+    @Override
+    public void fromAdmin(Message message, SessionID sessionID) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, RejectLogon {
+        System.out.println("Admin Message Received (Initiator) :" + message.toString());
+    }
+
+    @Override
+    public void toApp(Message message, SessionID sessionID) throws DoNotSend {
+
+    }
+
+    @Override
+    public void fromApp(Message message, SessionID sessionID) throws FieldNotFound, IncorrectDataFormat, IncorrectTagValue, UnsupportedMessageType {
+        System.out.println("Application Response Received (Initiator) :" +  message.toString());
+
+    }
+}
